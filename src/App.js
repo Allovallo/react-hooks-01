53184927b4085330b700c8566934edd84fae7204
Allovallo <30566134+Allovallo@users.noreply.github.com>
2024-07-05 17:27:22 +0300

    files updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Router, Route, Routes } from 'react-router-dom';
+
 import OldSignupForm from './components/SignupForm/SignupForm';
 
 const containerStyles = {
