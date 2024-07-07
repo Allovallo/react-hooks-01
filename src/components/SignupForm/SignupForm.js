ac5dace8118929863180a1a0f794e1392b9885a2
Allovallo <30566134+Allovallo@users.noreply.github.com>
2024-07-07 11:08:12 +0300

    SignupForm updated

--- src/components/SignupForm/SignupForm.js
+++ src/components/SignupForm/SignupForm.js
@@ -2,30 +2,36 @@ import styles from './SignupForm.module.css';
 import { useState } from 'react';
 
 export default function SignupForm() {
-  const [state, setState] = useState({
-    email: '',
-    password: '',
-  });
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  const handleEmailChange = event => {
-    // setEmail(event.target.value);
+  const handleChange = event => {
+    const { name, value } = event.target;
+
+    switch (name) {
+      case 'email':
+        setEmail(value);
+        break;
+
+      case 'password':
+        setPassword(value);
+        break;
+
+      default:
+        return;
+    }
   };
 
   return (
     <form className={styles.form} autoComplete="off">
       <label className={styles.label}>
         <span>Пошта</span>
-        <input type="email" name="email" onChange={handleEmailChange} value={email} />
+        <input type="email" name="email" onChange={handleChange} value={email} />
       </label>
 
       <label className={styles.label}>
         <span>Пароль</span>
-        <input
-          type="password"
-          name="password"
-          onChange={this.handleChange}
-          value={this.state.password}
-        />
+        <input type="password" name="password" onChange={handleChange} value={password} />
       </label>
 
       <button type="submit">Зарегистрироваться</button>
