f484a8e6b2c62ba60043aa18d558113e47de0542
Allovallo <30566134+Allovallo@users.noreply.github.com>
2024-07-10 18:05:28 +0300

    files updated

--- src/components/Clock/Clock.js
+++ src/components/Clock/Clock.js
@@ -1,40 +1,65 @@
+import { useState, useEffect, useRef } from 'react';
 import styles from './Clock.module.css';
-import React, { Component } from 'react';
 
-export default class OldClock extends Component {
-  state = {
-    time: new Date(),
-  };
+export default function Clock() {
+  const [time, setTime] = useState(() => new Date());
+  const intervalId = useRef(null);
 
-  intervalId = null;
-
-  componentDidMount() {
-    this.intervalId = setInterval(() => {
+  useEffect(() => {
+    intervalId.current = setInterval(() => {
       console.log('Це інтервал кожні 1000ms ' + Date.now());
-      this.setState({ time: new Date() });
+      setTime(new Date());
     }, 1000);
-  }
+  }, []);
 
-  componentWillUnmount() {
-    console.log('Цей метод визивається перед розмонтуванням компоненту');
-    this.stop();
-  }
-
-  stop = () => {
-    clearInterval(this.intervalId);
+  const stop = () => {
+    clearInterval(intervalId.current);
   };
 
-  render() {
-    return (
-      <div className={styles.container}>
-        <p className={styles.clockface}>
-          Поточний час:
-          {this.state.time.toLocaleTimeString()}
-        </p>
-        <button type="button" onClick={this.stop}>
-          Зупинити
-        </button>
-      </div>
-    );
-  }
+  return (
+    <div className={styles.container}>
+      <p className={styles.clockface}>Поточний час: {time.toLocaleTimeString()}</p>
+      <button type="button" onClick={stop}>
+        Зупинити
+      </button>
+    </div>
+  );
 }
+
+// class OldClock extends Component {
+//   state = {
+//     time: new Date(),
+//   };
+
+//   intervalId = null;
+
+//   componentDidMount() {
+//     this.intervalId = setInterval(() => {
+//       console.log('Це інтервал кожні 1000ms ' + Date.now());
+//       this.setState({ time: new Date() });
+//     }, 1000);
+//   }
+
+//   componentWillUnmount() {
+//     console.log('Цей метод визивається перед розмонтуванням компоненту');
+//     this.stop();
+//   }
+
+//   stop = () => {
+//     clearInterval(this.intervalId);
+//   };
+
+//   render() {
+//     return (
+//       <div className={styles.container}>
+//         <p className={styles.clockface}>
+//           Поточний час:
+//           {this.state.time.toLocaleTimeString()}
+//         </p>
+//         <button type="button" onClick={this.stop}>
+//           Зупинити
+//         </button>
+//       </div>
+//     );
+//   }
+// }
